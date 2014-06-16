b09f9a29f08bfb5043e459cd7660a8b95e47a1a5
Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
2014-06-16 16:47:42 -0400

    add: banks servicegroup

--- scripts/banks.py
+++ scripts/banks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 BANKS = {
-    'RBC banque royale': {
+    'RBC_banque_royale': {
         'protocol': 'https',
         'domain': 'www1.royalbank.com',
         'path': '/cgi-bin/rbaccess/rbunxcgi?F6=1&F7=IB&F21=IB&F22=IB&REQUEST=ClientSignin&LANGUAGE=FRENCH',
         },
-    'TD Canada Trust': {
+    'TD_Canada_Trust': {
         'protocol': 'https',
         'domain': 'banquenetcpo.td.com',
         'path': '/waw/idp/login.htm?execution=e1s1',
@@ -21,7 +21,7 @@ BANKS = {
         'domain': 'www.cibc.com',
         'path': '/ca/personal.html?int_id=HP_PersonalBanking',
         },
-    'Banque de Montreal': {
+    'Banque_de_Montreal': {
         'protocol': 'https',
         'domain': 'www1.bmo.com',
         'path': '/onlinebanking/cgi-bin/netbnx/NBmain?product=6',
@@ -36,7 +36,7 @@ BANKS = {
         'domain': 'bvi.bnc.ca',
         'path': '/',
         },
-    'Banque Laurentienne': {
+    'Banque_Laurentienne': {
         'protocol': 'https',
         'domain': 'blcweb.banquelaurentienne.ca',
         'path': '/BLCDirect/',
@@ -54,26 +54,52 @@ prefix = (
        host_name                banks
        alias                    banks
 }
+
+define servicegroup {
+	  servicegroup_name       group_banks
+	  alias                   Banks
+	  }
 """)
 
 template = (
 """define service {
+       # not working yet, bug reported on Shinken:
+       # https://github.com/naparuba/shinken/issues/1218#issuecomment-46223079
        use                      generic-service
        host_name                banks
-       service_description      Check http for %(bank)s online services
-       alias                    %(bank)s
+       alias                    Check http for %(bank)s online services
+       service_description      %(bank)s
+       labels                   group_banks
        check_command            check_http_service!%(domain)s!%(path)s%(more_options)s
 }
 """)
 
+business_rule = (
+"""
+define service {
+       use                            generic-service
+       host_name                      banks
+       service_description            Banks
+       # check_command                  bp_rule!g:group_banks
+       check_command                  bp_rule!%(all_banks)s
+       business_rule_output_template  Cassé: $($SERVICE_DESCRIPTION$ )$
+}
+""")
+
+
 
 def main():
     print prefix
+    # all_banks is a workaround while we wait for g:group_banks to work
+    all_banks = []
     for bank, values in BANKS.iteritems():
+        all_banks.append('banks,%s' % bank)
         print template % {'bank': bank,
                           'domain': values['domain'],
                           'path': values['path'],
                           'more_options': '!--ssl' if values['protocol'] == 'https' else ''}
+    all_banks = '&'.join(all_banks)
+    print business_rule % {'all_banks': all_banks}
 
 if __name__ == '__main__':
     main()
