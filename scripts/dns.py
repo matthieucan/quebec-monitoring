4578d6aab6bc3250f713fb93db244f0adde29de8
Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
2014-06-20 17:41:31 -0400

    update: dns config

--- scripts/dns.py
+++ scripts/dns.py
@@ -3,15 +3,15 @@
 
 DNS = {
     # Cogeco Cable (Trois-rivieres) 
-    'cogeco': ['205.151.69.200','205.151.68.200'],
+    'cogeco.ca': ['205.151.69.200','205.151.68.200'],
     
     # Sympatico.ca (BELL)
-    'sympatico': ['198.235.216.110', '209.226.175.224', '206.47.244.90',
+    'sympatico.ca': ['198.235.216.110', '209.226.175.224', '206.47.244.90',
                   '206.47.244.53', '206.47.244.54', '198.235.216.1',
                   '198.235.216.2', '198.235.216.130', '198.235.216.131'],
     
     # Telus
-    'telus': ['142.169.1.16', '199.84.242.22'],
+    'telusquebec.com': ['142.169.1.16', '199.84.242.22'],
     
     # Videotron.CA 
     'videotron.ca': ['205.151.222.250', '205.151.222.251', '24.200.241.2',
@@ -23,14 +23,14 @@ DNS = {
     'videotron.net': ['205.151.222.254', '205.151.222.253'],
     
     # Colbanet
-    'colbanet': ['216.252.64.75', '216.252.64.76', '69.28.239.8',
+    'colba.net': ['216.252.64.75', '216.252.64.76', '69.28.239.8',
                  '74.116.184.9', '69.28.239.9', '74.116.184.8'],
 
     # Cooptel
-    'cooptel': ['216.144.115.241', '216.144.115.242'],
+    'cooptel.qc.ca': ['216.144.115.241', '216.144.115.242'],
 
     # Fido
-    'fido': ['204.92.15.211', '204.92.15.212', '64.71.255.205', '64.71.255.253']
+    'fido.ca': ['204.92.15.211', '204.92.15.212', '64.71.255.205', '64.71.255.253']
     }
 
 
@@ -38,26 +38,28 @@ prefix = (
 """
 """)
 
-template = (
+template_host = (
 """
 define host {
        use                      generic-host
-       host_name                %(host)s_%(ip)s
-       address                  %(ip)s
-       alias                    %(host)s_%(ip)s
+       host_name                %(host)s
+       address                  %(host)s
+       alias                    %(host)s
        check_command            check_dummy!0!OK
 }
+""")
+
+template_service = (
+"""
 define service {
        use                      generic-service
-       host_name                %(host)s_%(ip)s
+       host_name                %(host)s
        check_command            check_dig_service!%(ip)s!www.gouv.qc.ca
        display_name             %(host)s (%(ip)s)
-       service_description      %(host)s_%(ip)s
+       service_description      %(ip)s
        servicegroups            group-dns
        notes                    order_%(order)d
-
 }
-
 """)
 
 postfix = (
@@ -87,9 +89,10 @@ def main():
     all_dns = []
     order = 1
     for host, ips in DNS.iteritems():
+        print template_host % {'host': host}
         for ip in ips:
-            print template % {'host': host, 'ip': ip, 'order': order}
-            all_dns.append('%(host)s_%(ip)s,%(host)s_%(ip)s' % {'host': host, 'ip': ip})
+            print template_service % {'host': host, 'ip': ip, 'order': order}
+            all_dns.append('%(host)s,%(ip)s' % {'host': host, 'ip': ip})
             order += 1
     print postfix % {'all_dns': '&'.join(all_dns)}
         
