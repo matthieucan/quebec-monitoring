39a23ffc0d208dbe3cdd61ac1a2c0ed90a81fb6f
Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
2014-06-23 14:22:35 -0400

    add: tokens.py, and .gitignore which comes along

---
+++ scripts/tokens.py
@@ -0,0 +1,10 @@
+# This file contains tokens, for data sources which require one.
+# If you don't want to sign-up to these services, leave this empty,
+# and the configuration for these particular services won't be generated.
+
+# Don't add your tokens here, but instead copy this file to `tokens.local.py`,
+# in order to keep your git status clean.
+
+TOKENS = {
+    'TRANSPORTS_AMT': '',
+}
