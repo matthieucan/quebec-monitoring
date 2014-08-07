5bf173d7cf7e913198d80c97089ba5dac31a3eb7
Matthieu Caneill <matthieu.caneill@savoirfairelinux.com>
2014-08-07 16:23:17 -0400

    angular app, first skeleton

--- app/app/js/controllers.js
+++ app/app/js/controllers.js
@@ -3,9 +3,28 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  /* home page */
+  .controller('HomeCtrl', ['$scope', '$http',
+    function($scope, $http) {
+      /* all boxes information */
+      $http.get('/adagios/rest/status/json/services/?fields=host_name,display_name,state,icon_image,plugin_output,&groups__has_field=main').success(function(data) {
+        $scope.boxes = data;
+      });
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
 
+  /* details view page */
+  .controller('DetailsCtrl', ['$scope', '$routeParams', '$http',
+    function($scope, $routeParams, $http) {
+      
+      /* box general information */
+      $http.get('/adagios/rest/status/json/services/?fields=display_name,state,icon_image&host_name=' + $routeParams.name).success(function(data) {
+        $scope.box = data;
+      });
+      
+      /* elements in this box */
+      $http.get('/adagios/rest/status/json/services/?fields=display_name,state,plugin_output,action_url&groups__has_field=' + $routeParams.name).success(function(data) {
+        $scope.elements = data;
+      });
+      
   }]);
