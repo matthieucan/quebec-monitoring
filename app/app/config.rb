ec4d6d0bab7e4a0b917c41ac4fb112867f5b56dd
David Barbeau <david.bardeau@savoirfairelinux.com>
2014-12-31 14:21:20 -0500

    [ #59973 ] [Frontend] Corrections & ajustements CSS/HTML. + transformation en SASS

---
+++ app/app/config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed # :compressed, :expanded
+line_comments = false
