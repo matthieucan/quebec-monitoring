ca6ed499c8f8641bfdb6e2a996b7cdad3dfb0024
David Barbeau <david.bardeau@savoirfairelinux.com>
2015-01-07 17:07:59 -0500

    [ #59973 ] [Frontend] Corrections & ajustements CSS/HTML/JS

--- app/app/js/transformations.js
+++ app/app/js/transformations.js
@@ -5,6 +5,32 @@ $(document).ready(function(){
 		  	changeForSvg(targetClass);
 		});
 	}
+
+	$(document).on('open', '.remodal', function () {
+    console.log('open');
+});
+
+$(document).on('opened', '.remodal', function () {
+    console.log('opened');
+});
+
+$(document).on('close', '.remodal', function () {
+    console.log('close');
+});
+
+$(document).on('closed', '.remodal', function () {
+    console.log('closed');
+});
+
+$(document).on('confirm', '.remodal', function () {
+    console.log('confirm');
+});
+
+$(document).on('cancel', '.remodal', function () {
+    console.log('cancel');
+});
+
+
 });
 
 function changeForSvg(oneImage){
